4bb653e7421a98f555cf693fc324167f157303aa
Chao-Wei, Peng <alanwei1202@gmail.com>
2018-10-03 22:28:27 +0800

    edit viewmodel

---
+++ ecgPaperDraw/ViewModels/PaperViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ecgPaperDraw.Models;
+
+namespace ecgPaperDraw.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+    public class PaperViewModel : ViewModelBase
+    {
+        private Paper _paper;
+        public PaperViewModel(double pixelsOfSmallBlock = 5.0)
+        {
+            _paper = new Paper();
+            _paper.smallBlock = pixelsOfSmallBlock;
+            _paper.bigBlock = _paper.smallBlock * 5;
+        }
+        public double SmallBlock
+        {
+            get { return _paper.smallBlock; }
+            set { _paper.smallBlock = value; OnPropertyChanged(); }
+        }
+        public double BigBlock
+        {
+            get { return _paper.bigBlock; }
+            set { _paper.bigBlock = value; OnPropertyChanged(); }
+        }
+    }
+}
